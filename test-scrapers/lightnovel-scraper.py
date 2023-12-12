be091d94cc28f1836ef3fff948cfd7722dc9f31e
Jaeyy166 <ojaysevilla64@gmail.com>
2023-12-12 12:46:50 +0800

    new scraper

--- test-scrapers/lightnovel-scraper.py
+++ test-scrapers/lightnovel-scraper.py
@@ -1,9 +1,18 @@
+# Book Walker Shoujo Light Novels web scraping practice
+# with tags: Slice of life, Romance and Shoujo
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-html = urlopen('https://shopee.ph/search?category=11044709&keyword=japanese%20light%20novel&subcategory=11046717&thirdCategory=11046757')
+html = urlopen("https://global.bookwalker.jp/categories/2/?qtag=6%2C1056%2C1066")
 soup = BeautifulSoup(html.read(), 'html.parser')
 
-item_name = soup.body.find_all('div', {'class':'GDO2sl_6Q4fcE fJEFbf'})
-for items in item_name:
-    print(items.get_text())
+h2 = soup.find_all('h2', {'class':'a-tile-ttl'})
+price = soup.find_all('div',{'class':'a-tile-price'})
+
+product_list = []
+
+for title in h2:
+    name = title.get_text().strip().replace('\n', '')
+    product_list.append(name)
+
+print(product_list)
