6371400328b244ed08e4f2988f720b0407b743c3
Jaeyy166 <ojaysevilla64@gmail.com>
2023-12-16 21:42:38 +0800

    new web scraping mini project

--- test-scrapers/lightnovel-scraper.py
+++ test-scrapers/lightnovel-scraper.py
@@ -3,19 +3,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get("https://global.bookwalker.jp/categories/2/?qtag=6%2C1056%2C1066")
+source = requests.get(
+        "https://global.bookwalker.jp/categories/2/?qtag=6%2C1056%2C1066"
+    )
 try:
     soup = BeautifulSoup(source.text, "html.parser")
 
-    mangas = soup.find_all("li", class_="o-tile")
+    mangas = soup.find_all('li', class_="o-tile")
 
     for manga in mangas:
         title = manga.find("h2", class_="a-tile-ttl")
         price = manga.find("div", class_="a-tile-price")
 
         cleanData = title.get_text() + price.get_text()
-        print(cleanData)
-        # AINT NO FUCKING WAY THIS WORKED
+        print(cleanData)    
+        # AINT NO FUCKING WAY THIS WORKED 
 
 except Exception as e:
     print(e)
---
+++ test-scrapers/repo-scraper.py
@@ -0,0 +1,4 @@
+# scraping my github repos
+# im going to try if i can get both name of repo and links
+from bs4 import BeautifulSoup
+import requests
