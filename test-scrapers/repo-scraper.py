9e3cf814b36a17b0c7217b8c33c7182707a7e1f2
Jaeyy166 <ojaysevilla64@gmail.com>
2023-12-17 22:29:22 +0800

    initial code structure

--- test-scrapers/repo-scraper.py
+++ test-scrapers/repo-scraper.py
@@ -2,3 +2,15 @@
 # im going to try if i can get both name of repo and links
 from bs4 import BeautifulSoup
 import requests
+
+source = 'http://#'
+r = requests.get(source)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+def get_links():
+
+
+def repo_lists():
+
+
+def main():
